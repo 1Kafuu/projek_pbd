052e2923a3586c51711de4df563fcab49310fc16
Vicky Candra Arifian <varifian47@gmail.com>
2025-10-15 11:38:36 +0700

    Login autentikasi edit

---
+++ app/Models/User.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Illuminate\Support\Facades\DB;
+
+class User extends Authenticatable 
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable, TwoFactorAuthenticatable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $table = 'user';
+    protected $primaryKey = 'iduser';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'username',
+        'password',
+        'idrole'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'two_factor_secret',
+        'two_factor_recovery_codes',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'password' => 'hashed',
+        ];
+    }
+
+    /**
+     * Get the user's initials
+     */
+    public function initials(): string
+    {
+        return Str::of($this->username)
+            ->explode(' ')
+            ->take(2)
+            ->map(fn ($word) => Str::substr($word, 0, 2))
+            ->implode('');
+    }
+
+    public function hasAnyRole(): bool
+    {
+        return in_array($this->idrole, [1, 2]);
+    }
+}
